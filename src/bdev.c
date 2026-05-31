2261cb991723c788411dd090dbc2e28ba8522d23
Beau Constrictor <dobbob11@duck.com>
2026-05-31 13:42:11 +0100

    fixed a involving switching between devices

--- src/bdev.c
+++ src/bdev.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "z80.h"
 
@@ -10,7 +12,8 @@
 typedef enum {
   bdev_t_nodev = 0,
   bdev_t_sectd = 1,
-  bdev_t_xmem  = 2
+  bdev_t_xmem  = 2,
+  bdev_t_rtcm  = 3
 } bdev_type;
 
 typedef struct {
@@ -23,12 +26,24 @@ typedef struct {
   byte sector;
 } bdev_xmem;
 
+typedef struct {
+  bool is_duration_mode;
+  bool pause;
+  uint64_t duration_timestamp;
+} bdev_rtc;
+
+typedef struct {
+  bdev_rtc rtcs[256];
+  byte profile;
+} bdev_rtcm;
+
 struct bdev_dev {
   bdev_type kind;
   bool bootable;
   union {
     bdev_sectd ss;
     bdev_xmem  xm;
+    bdev_rtcm  cm;
   };
 };
 
@@ -47,6 +62,8 @@ static void bdev_error(const char *s) {
   exit(1);
 }
 
+// setd:
+
 static byte bdev_sectd_read(bdev_sectd *dev, byte addr) {
   return dev->buffer[addr];
 }
@@ -64,8 +81,11 @@ static void bdev_sectd_flush(bdev_sectd *dev, byte sector) {
 
 static void bdev_sectd_setsect(bdev_sectd *dev, byte new) {
   fseek(dev->image, 0x100 * new, SEEK_SET);
+  memset(dev->buffer, '\0', 256);
   fread(dev->buffer, 1, 0x100, dev->image);
 }
+
+// xmem:
 
 static byte bdev_xmem_read(bdev_xmem *dev, byte addr) {
   return dev->memory[(dev->sector << 8) | addr];
@@ -79,6 +99,49 @@ static void bdev_xmem_setsect(bdev_xmem *dev, byte new) {
   dev->sector = new;
 }
 
+
+// rtcm:
+
+static uint64_t now_ms() {
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  return (uint64_t)ts.tv_sec  * 1000ULL +
+         (uint64_t)ts.tv_nsec / 1000000ULL;
+}
+
+static byte bdev_rtcm_read(bdev_rtcm *rtcm, byte addr) {
+  bdev_rtc *rtc = &rtcm->rtcs[rtcm->profile];
+
+  if (!rtc->is_duration_mode)
+    bdev_error("date mode is not yet supported");
+
+  uint64_t duration = now_ms() - rtc->duration_timestamp;
+  
+  switch (addr) {
+  case 0xff:
+    // the emulator supports all rtcs.
+    return 0xff;
+  case 0x00:
+    return rtc->pause ? 0xff : 0x00;
+  case 0x01:
+    return (word)(duration % 1000) & 0xff;
+  case 0x02:
+    return (word)(duration % 1000) >> 8;
+  }
+
+  return 0x00;
+}
+
+static void bdev_rtcm_setsect(bdev_rtcm *rtcm, byte new) {
+  rtcm->profile = new;
+}
+
+static void bdev_rtcm_write(bdev_rtcm *rtcm, word addr, byte val) {
+  (void)rtcm;
+  (void)addr;
+  (void)val;
+}
+
 static void bdev_flush(bdev_dev *dev, byte sector) {
   switch (dev->kind) {
   case bdev_t_nodev:
@@ -89,11 +152,14 @@ static void bdev_flush(bdev_dev *dev, byte sector) {
     break;
   case bdev_t_xmem:
     break; // you don't need to flush extended memory
+  case bdev_t_rtcm:
+    break; // neither do you have to flush a rtc
   }
 }
 
-static void bdev_setsect(bdev_devs *devs, bdev_dev *dev, byte new) {
-  bdev_flush(dev, devs->sector);
+static void bdev_setsect(bdev_devs *devs, bdev_dev *dev, byte new, bool flush) {
+  if (flush)
+    bdev_flush(dev, devs->sector);
 
   // allows a manual flush by switching to the sector your already
   // in
@@ -110,6 +176,8 @@ static void bdev_setsect(bdev_devs *devs, bdev_dev *dev, byte new) {
   case bdev_t_xmem:
     bdev_xmem_setsect(&dev->xm, new);
     break;
+  case bdev_t_rtcm:
+    bdev_rtcm_setsect(&dev->cm, new);
   }
 
   devs->sector = new;
@@ -134,21 +202,17 @@ static byte bdev_read(void *state, word addr) {
   bdev_devs *devs = (bdev_devs*)state;
   bdev_dev *dev = devs->devices[devs->selected];
 
-  if (addr == devs->status_s) {
+  if (addr == devs->status_s)
     return bdev_status(devs);
-  }
 
-  if (addr == devs->sect_s) {
+  if (addr == devs->sect_s) 
     return devs->sector;
-  }
 
-  if (addr == devs->dev_s) {
+  if (addr == devs->dev_s)
     return devs->selected;
-  }
 
-  if (dev == NULL) {
+  if (dev == NULL)
     bdev_error("attempted read to an empty device slot");
-  }
 
   byte block_addr = addr - devs->block_s;
   switch (dev->kind) {
@@ -159,6 +223,8 @@ static byte bdev_read(void *state, word addr) {
     return bdev_sectd_read(&dev->ss, block_addr);
   case bdev_t_xmem:
     return bdev_xmem_read(&dev->xm, block_addr);
+  case bdev_t_rtcm:
+    return bdev_rtcm_read(&dev->cm, block_addr);
   }
 }
 
@@ -172,7 +238,7 @@ static void bdev_write(void *state, word addr, byte val) {
     devs->selected = val;
     dev = devs->devices[val];
     if (dev != NULL)
-      bdev_setsect(devs, dev, devs->sector); // load sector on new device
+      bdev_setsect(devs, dev, devs->sector, false); // load sector on new device
     return;
   }
 
@@ -184,7 +250,7 @@ static void bdev_write(void *state, word addr, byte val) {
   }
 
   if (addr == devs->sect_s) {
-    bdev_setsect(devs, dev, val);
+    bdev_setsect(devs, dev, val, true);
     return;
   }
 
@@ -198,6 +264,9 @@ static void bdev_write(void *state, word addr, byte val) {
     break;
   case bdev_t_xmem:
     bdev_xmem_write(&dev->xm, block_addr, val);
+    break;
+  case bdev_t_rtcm:
+    bdev_rtcm_write(&dev->cm, block_addr, val);
     break;
   }
 }
